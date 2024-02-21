17029eab2339d239fd7be8a3e69f6dcb9b32d586
n0n0n0f <r4sl3xgod@gmail.com>
2024-02-22 02:06:56 +0700

    Написал css стили для глаз

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -47,4 +47,149 @@ export default {
 </script>
 
 <style scoped>
+.shopping-cart {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.cart-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.back-link {
+  font-size: 18px;
+  color: #fff;
+  text-decoration: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: #8a2be2;
+  transition: background-color 0.3s ease;
+}
+
+.back-link:hover {
+  background-color: #9400d3;
+}
+
+.back-link:active {
+  background-color: #6a5acd;
+}
+
+.empty-msg {
+  font-size: 24px;
+  color: #6a0dad;
+  text-align: center;
+}
+
+.cart-items {
+  margin-bottom: 20px;
+}
+
+.item {
+  padding: 20px;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.item-details {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.item-info {
+  flex: 1;
+}
+
+.item-title {
+  font-size: 22px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 5px;
+}
+
+.item-description {
+  font-size: 14px;
+  color: #666;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.item-price {
+  font-size: 18px;
+  color: #333;
+  margin-bottom: 10px;
+}
+
+.price {
+  border: 1px solid #6a0dad;
+  padding: 2px 5px;
+  border-radius: 4px;
+}
+
+.quantity-actions {
+  display: flex;
+  align-items: center;
+}
+
+.quantity-button {
+  font-size: 18px;
+  color: #fff;
+  background-color: #8a2be2;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  padding: 8px 12px;
+  margin: 0 5px;
+  transition: background-color 0.3s ease;
+}
+
+.quantity-button:hover {
+  background-color: #9400d3;
+}
+
+.quantity {
+  font-size: 18px;
+  color: #6a0dad;
+  margin: 0 10px;
+}
+
+.quantity-value {
+  color: #6a0dad;
+}
+
+.delete-button {
+  font-size: 18px;
+  color: #fff;
+  background-color: #e74c3c;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  padding: 10px 20px;
+  transition: background-color 0.3s ease;
+}
+
+.delete-button:hover {
+  background-color: #865dfd;
+}
+
+.order-button {
+  display: block;
+  margin: 0 auto;
+  font-size: 20px;
+  color: #fff;
+  text-decoration: none;
+  background-color: #6a0dad;
+  padding: 12px 24px;
+  border-radius: 8px;
+  transition: background-color 0.3s ease;
+}
+
+.order-button:hover {
+  background-color: #450d5f;
+}
 </style>
