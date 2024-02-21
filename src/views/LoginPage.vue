87829e337b0959819793b02f16fe42d039d6f3ff
n0n0n0f <r4sl3xgod@gmail.com>
2024-02-22 02:06:58 +0700

    Написал css стили для глаз

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -30,5 +30,80 @@ export default {
 }
 </script>
 <style scoped>
+.form-header {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
 
+.login-form {
+  flex-direction: column;
+  justify-content: center;
+  width: 400px;
+  margin: 0 auto;
+  display: flex;
+}
+
+.label {
+  font-size: 22px;
+  margin-bottom: 5px;
+  color: #8a2be2;
+}
+
+.input-field {
+  font-size: 20px;
+  height: 45px;
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  transition: border-color 0.3s ease;
+  background-color: #e6e6fa;
+}
+
+.input-field:focus {
+  border-color: #8a2be2;
+}
+
+.error {
+  border-color: red;
+}
+
+.error-message {
+  color: red;
+  margin-top: -10px;
+}
+
+.submit-button {
+  cursor: pointer;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  background-color: #8a2be2;
+  padding: 12px 20px;
+  font-size: 22px;
+  transition: background-color 0.3s ease;
+}
+
+.submit-button:hover {
+  background-color: #7e349d;
+}
+
+.prevPage {
+  font-size: 22px;
+  color: #fff;
+  text-decoration: none;
+  align-items: center;
+  justify-content: center;
+  background-color:#8a2be2;
+  display: flex;
+  width: 160px;
+  height: 50px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.prevPage:hover {
+  background-color: #7e349d;
+}
 </style>
