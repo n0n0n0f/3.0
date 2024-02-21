2abd385d3ba24a0c0c962b15c9044d6b4b0f06f1
n0n0n0f <r4sl3xgod@gmail.com>
2024-02-22 02:07:02 +0700

    Написал css стили для глаз

--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -41,5 +41,88 @@ export default {
 </script>
 
 <style scoped>
+.form-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #f7f7f7;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
 
+.form-header {
+  margin-bottom: 20px;
+}
+
+.registration-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.label {
+  font-size: 22px; /* Увеличенный размер текста для меток */
+  margin-bottom: 5px; /* Немного увеличиваем расстояние между метками и полями */
+}
+
+.input-field {
+  font-size: 20px;
+  height: 45px;
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  transition: border-color 0.3s ease;
+  background-color: #e6e6fa;
+}
+
+.input-field:hover {
+  background-color: #d8bfd8;
+}
+
+.input-field:focus {
+  border-color: #8a2be2;
+}
+
+.error {
+  border-color: red;
+}
+
+.error-message {
+  color: red;
+  margin-top: -10px;
+}
+
+.submit-button {
+  cursor: pointer;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  background-color: #8a2be2;
+  padding: 12px 20px;
+  font-size: 22px; /* Увеличенный размер текста для кнопки */
+  transition: background-color 0.3s ease;
+}
+
+.submit-button:hover {
+  background-color: #7e349d;
+}
+
+.prevPage {
+  font-size: 22px; /* Увеличенный размер текста для кнопки */
+  color: #fff;
+  text-decoration: none;
+  align-items: center;
+  justify-content: center;
+  background-color: #8a2be2;
+  display: flex;
+  width: 160px;
+  height: 50px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-left: 30%;
+}
+
+.prevPage:hover {
+  background-color: #7e349d;
+}
 </style>
