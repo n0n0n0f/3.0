a5000ee27e0e8f216e2c3dfdc229b72369d883c9
n0n0n0f <r4sl3xgod@gmail.com>
2024-02-22 02:07:00 +0700

    Написал css стили для глаз

--- src/views/CatalogPage.vue
+++ src/views/CatalogPage.vue
@@ -49,5 +49,95 @@ export default {
 </script>
 
 <style scoped>
+.products {
+  max-width: 1200px;
+  margin: 0 auto;
+}
 
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.nav-links {
+  background-color: #7e349d;
+  height: 40px;
+  border-radius: 10px;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  transition: background-color 0.3s ease; /* Добавляем анимацию */
+}
+
+.nav-links * {
+  text-decoration: none;
+  color: #fff;
+}
+
+.nav-links:hover {
+  background-color: #8a2be2;
+}
+
+.product-list {
+  margin-top: 40px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.product {
+  border: 1px solid #8a2be2;
+  border-radius: 8px;
+  width: 30%;
+  padding: 20px;
+  margin-bottom: 20px;
+  background-color: #e6e6fa;
+}
+
+.product h3 {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.product-description {
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 10px;
+}
+
+.product-price-container {
+  border: 1px solid #8a2be2;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+  transition: border-color 0.3s ease;
+}
+
+.product-price {
+  font-size: 18px;
+  color: #333;
+  margin: 0;
+}
+
+.product-price-container:hover {
+  border-color: #9b59b6;
+}
+
+.add-to-cart {
+  width: 100%;
+  height: 40px;
+  background-color: #8a2be2;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.add-to-cart:hover {
+  background-color: #7e349d;
+}
 </style>
